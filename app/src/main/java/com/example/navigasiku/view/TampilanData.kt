ce5ad3b6247980546872a52c8c3a67a68e9db452
Asti22 <uun3461@gmail.com>
2025-11-06 16:06:53 +0700

    Menambahkan Spacer sebagai jarak antar elemen dan tombol “Back” penuh lebar untuk navigasi kembali dengan aksi onBackBtnClick.

--- app/src/main/java/com/example/navigasiku/view/TampilanData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilanData.kt
@@ -2,8 +2,13 @@ package com.example.navigasiku.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,6 +21,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigasiku.R
 
@@ -51,6 +57,14 @@ fun TampilData(
                             fontFamily = FontFamily.Cursive, fontSize = 22.sp
                         )
                     }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(id = "Back"))
                 }
             }
         }
